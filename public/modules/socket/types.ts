5348073dcc7c6151f7f1e609b0d5e5c852e166cd
Oscar <56079903+OscarNOW@users.noreply.github.com>
2025-12-12 14:06:25 +0100

    Added socket types

---
+++ public/modules/socket/types.ts
@@ -0,0 +1,25 @@
+import type { Server, Socket } from 'socket.io';
+
+export interface ServerToClientEvents {
+    connect: () => void;
+    manyData: (data: unknown) => void;
+}
+
+export interface ClientToServerEvents {
+    connect: () => void;
+    subscribeMany: (model: string) => void;
+    subscribeSpecific: (model: string, id: string) => void;
+    unsubscribeMany: (model: string) => void;
+    unsubscribeSpecific: (model: string, id: string) => void;
+}
+
+export interface InterServerEvents {
+    // 
+}
+
+export interface SocketData {
+    // 
+}
+
+export type ioType = Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>;
+export type socketType = Socket<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>;
