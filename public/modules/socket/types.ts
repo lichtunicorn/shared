bd8d26b5ac91990d6e943a205402667d0524afda
Oscar <56079903+OscarNOW@users.noreply.github.com>
2025-12-13 13:59:22 +0100

    Added show socket events

--- public/modules/socket/types.ts
+++ public/modules/socket/types.ts
@@ -1,5 +1,6 @@
 import type { Server, Socket } from 'socket.io';
 import type { modelName, publicModelData } from '../database/structure/types';
+import type { showData } from '../show/types';
 
 export interface ServerToClientEvents {
     connect: () => void;
@@ -9,6 +10,8 @@ export interface ServerToClientEvents {
 
     manyData<T extends modelName>(model: T, data: publicModelData<T>[]): void;
     specificData<T extends modelName>(model: T, id: string, data: publicModelData<T> | null): void;
+
+    shows(): showData[];
 }
 
 export interface ClientToServerEvents {
@@ -19,6 +22,10 @@ export interface ClientToServerEvents {
     subscribeSpecific: (model: modelName, id: string) => void;
     unsubscribeMany: (model: modelName) => void;
     unsubscribeSpecific: (model: modelName, id: string) => void;
+
+    subscribeShows: () => void;
+    unsubscribeShows: () => void;
+    createShow: (showData: Omit<showData, 'id'>) => void;
 }
 
 export interface InterServerEvents {
@@ -27,7 +34,10 @@ export interface InterServerEvents {
 
 export interface SocketData {
     clientId: string;
+
     showId: string | null;
+    showsSubscribed: boolean;
+
     manySubscribedModels: modelName[];
     specificSubscribedModels: Partial<{
         [currentModelName in modelName]: string[]
