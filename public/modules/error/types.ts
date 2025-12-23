1b2fe444b2c34084f2b37150b20aca77314321db
Oscar <56079903+OscarNOW@users.noreply.github.com>
2025-12-23 18:15:45 +0100

    Added errorInfo.createdAt

--- public/modules/error/types.ts
+++ public/modules/error/types.ts
@@ -1,6 +1,7 @@
 import type { modelName } from "../database/structure/types";
 
 export type errorInfo = {
+    createdAt: number;
     error: Error;
     source: string | null;
     showId: string | null;
