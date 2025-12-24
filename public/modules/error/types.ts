18dec3454f3921393d8c833976b6c92c6930cec9
Oscar <56079903+OscarNOW@users.noreply.github.com>
2025-12-24 21:12:44 +0100

    Added errorInfo.count

--- public/modules/error/types.ts
+++ public/modules/error/types.ts
@@ -2,6 +2,7 @@ import type { modelName } from "../database/structure/types";
 
 export type errorInfo = {
     createdAt: number;
+    count: number;
     message: string;
     source: string | null;
     showId: string | null;
