548863eb9ea3403f8e3bf90cbb7ecbb8cc681780
Oscar <56079903+OscarNOW@users.noreply.github.com>
2026-01-18 15:39:41 +0100

    improved dev log

--- modules/database/structure/dev.ts
+++ modules/database/structure/dev.ts
@@ -30,7 +30,7 @@ function updateConstTypes() {
     const newTypesContents = `${fileStart}${startString}${constTypes}${endString}${fileEnd}`;
 
     if (newTypesContents !== typesContents) {
-        console.warn('Database structure types have changed inside shared. Run "bun u" inside shared and make a new commit');
+        console.warn('Database structure types updated');
 
         fs.writeFileSync(path.join(__dirname, 'types.ts'), newTypesContents);
     }
