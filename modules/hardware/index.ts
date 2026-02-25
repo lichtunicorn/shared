0dca27780cdcc18106f8d3a8d181bae381c36aac
Oscar <56079903+OscarNOW@users.noreply.github.com>
2026-02-25 12:51:47 +0100

    Added hardware arrays

--- modules/hardware/index.ts
+++ modules/hardware/index.ts
@@ -1,1 +1,71 @@
+import type { generalFunctionNames } from "../database/structure/models/actionButton";
+
 export const serialPaths = ['COM1', 'COM2', 'COM3', 'COM4'] as const;
+
+export const serverClickButtons = [
+    'clear',
+    'blind',
+    'highlight',
+    'previous',
+    'all',
+    'next',
+    'nextPage',
+    'previousPage',
+    'nextSection',
+] as const satisfies typeof generalFunctionNames[number][];
+
+export const clientClickButtons = [
+    'kindIntensity',
+    'kindPosition',
+    'kindColor',
+    'kindBeam',
+
+    'group',
+    'scene',
+    'cuelist',
+
+    'empty',
+    'copy',
+    'move',
+    'delete',
+
+    'select',
+    'record',
+    'setAttribute',
+
+    'go',
+    'release',
+    'assign',
+    'open',
+
+    'arrowLeft',
+    'arrowRight',
+    'arrowUp',
+    'arrowDown',
+
+    'number0',
+    'number1',
+    'number2',
+    'number3',
+    'number4',
+    'number5',
+    'number6',
+    'number7',
+    'number8',
+    'number9',
+    'dot',
+    'at',
+    'thru',
+    'plus',
+    'enter',
+    'backspace',
+    'set',
+] as const satisfies typeof generalFunctionNames[number][];
+
+export const clientHoldButtons = [
+    'section-1',
+    'section-2',
+    'section-3',
+    'section-4',
+    'section-5',
+] as const satisfies typeof generalFunctionNames[number][];
