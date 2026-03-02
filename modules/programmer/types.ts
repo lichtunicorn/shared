5169cde33cd3f1a830dbd3984211abcc91bb5562
Oscar <56079903+OscarNOW@users.noreply.github.com>
2026-03-02 19:59:59 +0100

    added values to programmerState

--- modules/programmer/types.ts
+++ modules/programmer/types.ts
@@ -2,8 +2,12 @@ import type z from "zod";
 import type { kind } from "../../kinds";
 import type { subKindSchema } from "../fixtureTypes/schema";
 
+export type param = z.infer<typeof subKindSchema> & {
+    values: number[] | string[] | boolean[];
+}
+
 export type programmerState = {
     chosenKind: kind;
     currentPage: number;
-    pages: z.infer<typeof subKindSchema>[][];
+    pages: param[][];
 };
